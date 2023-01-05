78c68ab12f9442398cf64ea3a92b13c87661be06
yoshiki <effort0217@icloud.com>
2023-01-05 23:18:04 +0900

    fix.router

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
 import HomePage from "./comp/HomePage";
-import Login from "./comp/Login";
-import { BrowserRouter } from "react-router-dom";
-import Router from "./comp/router/Router";
-import { Route } from "react-router-dom";
+import Routing from "./comp/router/Routing";
+
 
 const App = () => {
   return (
-    <div className="App">
-      <BrowserRouter>
-        
-          <Route path="/" element={<HomePage/>}>
-            <Route path='login' element={<Login/>} /> 
-          </Route>
-          
-        <Router />
-      </BrowserRouter>
+    <div className="App">               
+      <Routing />
     </div>
   );
 }
